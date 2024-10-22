c5888bd769694d240d4809b38dd3bd070eb0f4f7
Jessica Silva <jps936@hotmail.com>
2024-10-22 18:59:08 +0100

    refactor: change project structure

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import { useState, useEffect } from 'react';
 import type { Expense } from './types/expense';
 
-import getGoogleSheetsExpensesData from './utils/get-expenses-data';
+import getGoogleSheetsExpensesData from './hooks/get-expenses-data';
 
 export default function Home() {
   const [expenses, setExpenses] = useState<Expense[]>([]);
