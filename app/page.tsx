23e27edf478507746e71603333b993026ca851a2
Jessica Silva <jps936@hotmail.com>
2024-10-25 20:55:18 +0100

    feat: add date range picker component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 import { useState, useEffect } from 'react';
 import type { Expense } from './types/expense';
+import { subDays } from "date-fns"
 
 import getGoogleSheetsExpensesData from './hooks/get-expenses-data';
 
 import { MonthSelect } from '@/components/month-select';
+import DateRangePicker from '@/components/date-range-picker';
+import type { DateRange } from "react-day-picker"
 
 export default function Home() {
   const [expenses, setExpenses] = useState<Expense[]>([]);
@@ -12,6 +15,11 @@ export default function Home() {
   const [error, setError] = useState<string | null>(null);
   const [selectedMonth, setSelectedMonth] = useState<string>('');
   const [selectedYear, setSelectedYear] = useState<number>(2024);
+  const today = new Date();
+  const [date, setDate] = useState<DateRange>({
+    from: subDays(today, 30),
+    to: today,
+  })
 
   useEffect(() => {
     async function fetchExpensesData() {
@@ -37,6 +45,7 @@ export default function Home() {
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Expenses Overview</h1>
       <MonthSelect value={selectedMonth} selectedYear={selectedYear} onChange={(value) => setSelectedMonth(value)} />
+      <DateRangePicker date={date} onSelect={(value) => setDate(value as DateRange)} />
     </div>
   );
 }
